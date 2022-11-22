0b3f292584f0be18166f887ad282651f61eff0d3
Tatiana <t-b-k@bk.ru>
2022-11-22 19:20:12 +0300

    Task 4 from Homework 1 is finished

---
+++ HMW1_Task4_All_Even_upto_N/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая на вход принимает число N, 
+// а на выход выдает все четные числа от 0 до N
+
+Console.Clear(); 
+
+Console.Write("Введите целое положительное число N => "); 
+
+int number = int.Parse(Console.ReadLine()); 
+Console.WriteLine(); 
+
+int count = 0; 
+
+if (number < 0) 
+{
+    Console.WriteLine("Извините, Ваше число не является положительным."); 
+}
+else
+{
+    Console.Write("Вот вам все четные числа в промежутке от 0 до "); 
+    Console.Write(number);
+    Console.WriteLine(":"); 
+   
+    while (count <= number-2) 
+    {
+        Console.Write(count); 
+        Console.Write(", ");      
+        count=count+2;  
+    }
+
+    Console.WriteLine(count); 
+}
+
