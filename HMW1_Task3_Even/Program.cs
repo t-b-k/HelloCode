c34860892210ea8ff4fcfa45af9650d7da448637
Tatiana <t-b-k@bk.ru>
2022-11-22 18:19:34 +0300

    Task 3 from Homework 1 is done

---
+++ HMW1_Task3_Even/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход целое число
+// и в качестве результата сообщает, является ли оно четным
+
+Console.Clear();
+
+Console.Write("Введите целое число а => ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Число "); 
+Console.Write(a); 
+
+if (a%2==0) 
+    Console.WriteLine(" является четным."); 
+else
+    Console.WriteLine(" не является четным. "); 
+
+Console.WriteLine(); 
