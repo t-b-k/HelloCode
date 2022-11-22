057d2ebae541f857b8f1046d4e93336b588b1ede
Tatiana <t-b-k@bk.ru>
2022-11-22 16:36:25 +0300

    Task 1 from Homework 1 is done

---
+++ HMW1_Task1_Compare/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1
+// Напишите программу, которая на вход принимает два числа и выдает, 
+// какое из чисел больше, а какое меньше
+
+Console.Clear();
+
+Console.Write("Введите целое число а => ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите целое число b => ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine(); 
+Console.Write("Результат: ");
+
+if (a==b) 
+{
+    Console.WriteLine("Числа a и b равны"); 
+    Console.Write(a); 
+    Console.Write("  и ");
+    Console.Write(b); 
+    Console.WriteLine(" равны. "); 
+}
+else
+{
+    if (a>b) 
+    {
+        Console.Write("Число "); 
+        Console.Write(a); 
+        Console.Write("  больше числа ");
+        Console.Write(b); 
+        Console.WriteLine("."); 
+    }
+    else 
+    {
+        Console.Write("Число "); 
+        Console.Write(b); 
+        Console.Write("  больше числа ");
+        Console.Write(a); 
+        Console.WriteLine("."); 
+    }    
+}
+Console.WriteLine(); 
