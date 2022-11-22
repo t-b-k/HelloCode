9020eb57da83d63df11525b2156b87524c5b1e90
Tatiana <t-b-k@bk.ru>
2022-11-22 20:13:44 +0300

    Added Example 010 - searching definite number in array (index)

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// Имеется одномерный массив array из n элементов. 
+// Требуется найти элемент массива, равный find
+
+int[] array = {1, 12, 31, 4, 15, 23, 16, 15, 18}; 
+
+int n = array.Length; 
+int find = 15; 
+
+int index = 0; 
+
+while (index < n) 
+{
+    if (array[index] == find) 
+    {
+        Console.WriteLine(index); 
+        break; 
+    }
+    index++; 
+}
