1b1f1acc73ca066b4273829c14b988bc69ddb54b
Tatiana <t-b-k@bk.ru>
2022-11-24 21:01:52 +0300

    Added Example 2 and Example 3 from Seminar 2

---
+++ Seminar2_Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая выводит случайное 3-хзначное число
+// и удаляет из него вторую цифру
+
+Random random = new Random(); 
+int number = random.Next(100,1000);
+
+Console.WriteLine(number); 
+
+int firstNumber = number / 100; 
+int lastNumber = number % 10; 
+int result = firstNumber*10 + lastNumber; 
+
+Console.WriteLine(result); 
+
---
+++ Seminar2_Ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу, которая принимает на вход два числа
+// и выводит, является ли второе число кратным первому. 
+// Если второе число не кратно первому, то программа 
+// выводит остаток от деления
+
+Console.Write("Введите первое число (отличное от нуля) => "); 
+
+int firstNumber = int.Parse(Console.ReadLine()); 
+
+Console.Write("Введите второе число (отличное от нуля) => "); 
+
+int secondNumber = int.Parse(Console.ReadLine());
+
+int rest = secondNumber % firstNumber; 
+
+if (rest == 0) Console.WriteLine("Поздравляем! Второе число кратно первому.");
+else 
+{
+    Console.Write ("Второе число не кратно первому. Остаток от деления равен "); 
+    Console.WriteLine(rest); 
+} 
