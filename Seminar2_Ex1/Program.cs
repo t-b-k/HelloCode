13898f73c96a56d00a49a477a19b44b818a77451
Tatiana <t-b-k@bk.ru>
2022-11-24 20:25:05 +0300

    Added Example 1 from Seminar 2

---
+++ Seminar2_Ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая вводит случайное число в диапазоне [10;99] 
+// и показывает бОльшую из его цифр
+
+Random random = new Random(); 
+int number = random.Next(10,100);
+
+Console.WriteLine(number); 
+
+int firstNumber = number / 10; 
+int lastNumber = number % 10; 
+
+if (firstNumber > lastNumber) Console.WriteLine(firstNumber); 
+else Console.WriteLine(lastNumber); 
+
