b1cf47904dec0b12d84f5b2e53b27af6e3f1bd8f
Tatiana <t-b-k@bk.ru>
2022-11-25 17:21:34 +0300

    Task 3 from Homework 2 is added

---
+++ HMW2_Task3_Is_Weekend/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.Write("Введите номер дня недели (от 1 до 7) => "); 
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number); 
+
+if (!isParsed || number < 1 || number > 7) 
+{
+    Console.WriteLine("То, что Вы ввели, не является целым числом в диапазоне от 0 до 7");
+}
+else
+{
+    if (number == 6 || number == 7) 
+    {
+        Console.WriteLine("Это ВЫХОДНОЙ день"); 
+    }
+    else 
+    {
+        Console.WriteLine($"{number}-й день не является выходным"); 
+    }
+}    
+    
