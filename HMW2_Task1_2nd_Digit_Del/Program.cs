fd149e719e928ef645184045d09ec3f54bcbd324
Tatiana <t-b-k@bk.ru>
2022-11-25 17:24:55 +0300

    Task 1 from Homework 2 is added

---
+++ HMW2_Task1_2nd_Digit_Del/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру этого числа
+
+Console.Write("Введите трехзначное натуральное число => "); 
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number); 
+
+if (!isParsed) 
+{
+    Console.WriteLine("То, что Вы ввели, не является целым числом");
+    return; 
+}
+else
+{
+    if (number <= 0) 
+    {
+        Console.WriteLine("То, что Вы ввели, не является натуральным числом"); 
+        return; 
+    }
+    else
+    {
+        if (number < 100 || number > 999) 
+        {
+            Console.WriteLine("То, что Вы ввели, не является трехзначным натуральным числом"); 
+            return; 
+        }
+    }
+}
+
+int result = number / 10 % 10; 
+Console.WriteLine($"Второй цифрой в Вашем числе является цифра {result}"); 
