acc70553b6c5da7c94379b648dec55b8e354943b
Tatiana <t-b-k@bk.ru>
2022-11-28 21:18:52 +0300

    Added Example 3 from Seminar 3

---
+++ Sem3_Ex3_Distance/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход 
+// координаты двух точек и находит растояние между ними в 2D-пространстве
+
+// AB = V AC2 + BC2
+
+double inputDouble () 
+{
+    Console.WriteLine ("Введите целое число"); 
+    bool isNum = double.TryParse(Console.ReadLine(), out double num);
+    if (isNum) 
+    {
+        return num; 
+    } 
+    else 
+    {
+        Console.WriteLine("Вы ввели некорректное значение"); 
+        return -1;
+    }
+}
+
+double ax = inputDouble(); 
+double ay = inputDouble(); 
+double bx = inputDouble(); 
+double by = inputDouble(); 
+
+double ab = Math.Sqrt((bx-ax)*(bx-ax) + (by-ay)*(by-ay));  
+
+Console.WriteLine(ab); 
