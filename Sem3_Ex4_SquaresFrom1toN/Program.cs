b490e60bfd5576cfb7ca0ce048a0bb8ac19e0927
Tatiana <t-b-k@bk.ru>
2022-11-28 22:07:48 +0300

    Added Example 4 to Seminar 3

---
+++ Sem3_Ex4_SquaresFrom1toN/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает таблицу квадратов чисел от 1 до N
+
+int num = InputInt();
+int[] squaresTable = GetSquaresTable (num);
+PrintArray(squaresTable); 
+
+
+int InputInt () 
+{
+    Console.WriteLine("Введите целое число");
+    bool isNum = int.TryParse(Console.ReadLine(), out int num); 
+    if (isNum) 
+    {
+        return num; 
+    } 
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение"); 
+        return -1; 
+    }
+}
+
+int[] GetSquaresTable (int num) 
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++) 
+    {
+        array[i] = (int)((Math.Pow(i+1,2)));
+    } 
+    return array; 
+}
+
+void PrintArray (int[] arr) 
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} "); 
+    }
+    Console.WriteLine();
+}
