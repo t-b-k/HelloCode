c0f4d71b58764acbf5e5a3361c6fb61370089b6f
Tatiana <t-b-k@bk.ru>
2022-11-28 20:32:18 +0300

    Added Example 1 from Seminar 3 - Quadrant Definition

---
+++ Sem3_Ex1_Quadrant/Program.cs
@@ -0,0 +1,58 @@
+﻿// Напишите программу, которая принимает на вход координаты
+// точки (X, Y), причем X<>0 и Y<>0, и выдает номер четверти
+// в которой находится эта точка
+
+bool isParsedX = int.TryParse(Console.ReadLine(), out int x); 
+bool isParsedY = int.TryParse(Console.ReadLine(), out int y); 
+
+if (!isParsedX || !isParsedY)  
+{
+    Console.WriteLine("Координаты введены некорректно"); 
+    return;
+}
+
+// возвращаемый_тип_данных НазваниеМетода()
+
+int GetQuarterNumberByCoordinates (int x, int y)
+{
+    if (x > 0 && y > 0) 
+    {
+        return 1;
+    }
+
+    if (x < 0 && y > 0) 
+    {
+        return 2; 
+    }
+
+    if (x < 0 && y < 0) 
+    {
+        return 3;
+    }
+
+    if (x > 0 && y < 0) 
+    {
+        return 4; 
+    }
+
+return -1; 
+
+}
+
+int quarterNumber = GetQuarterNumberByCoordinates (x,y); 
+
+if (quarterNumber == -1) 
+{
+    Console.WriteLine("Координаты введены некорректно");
+    return;  
+} 
+
+Console.WriteLine(quarterNumber); 
+
+void PrintArray (int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.WriteLine(array[i]); 
+    }
+}
