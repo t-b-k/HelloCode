36906c090b6c8823de1775a0bb5dfa071bf4938c
Tatiana <t-b-k@bk.ru>
2022-12-01 17:28:58 +0300

    Added Task 3 from Homework 3 - Printing N cubes

---
+++ HMW3_Task3_N_Cubes/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая принимает на вход натуральное число N 
+// и выдает таблицу кубов чисел от 1 до N
+
+Console.Write("Введите натуральное число N => "); 
+
+int numberN = InputNatural(); 
+
+if (numberN > 0) 
+{
+    Console.WriteLine($"Вот как будет выглядеть последовательность кубов натуральных чисел от 1 до {numberN}: "); 
+    PrintArray(GetArrayOfCubes(numberN)); 
+}
+else
+{
+   Console.WriteLine("То, что Вы ввели, не является натуральным числом"); 
+   Console.WriteLine("Программа завершает свою работу."); 
+}
+
+void PrintArray (int[] array)
+{
+    int lastElement = array.Length-1; 
+    for (int i = 0; i < lastElement; i++) 
+    {
+       Console.Write($"{array[i]}, "); 
+    }
+    Console.WriteLine(array[lastElement]);  
+}
+
+int InputNatural () 
+{
+    bool isNatural = int.TryParse(Console.ReadLine(), out int n); 
+    if (!isNatural || n <= 0) 
+    {
+        return 0; 
+    }
+    else
+    {
+       return n;       
+    }
+}
+
+int[] GetArrayOfCubes (int n)
+{
+    int[] array = new int[n]; 
+    for (int j = 0; j < n; j++) 
+    {
+        array[j] = (int)Math.Pow(j+1, 3); 
+    }    
+    return array; 
+}
