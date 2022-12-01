34991d70b38fbb41b4bec2ea6909a6856cb7e9f9
Tatiana <t-b-k@bk.ru>
2022-12-01 22:37:45 +0300

    Added Example 1 to Seminar 4 - Counting q-ty of digits in the number

---
+++ Seminar4_Ex1_NumOfDigits/Program.cs
@@ -0,0 +1,17 @@
+﻿// Метод NumberOfDigits возвращает количество цифр в целом неотрицательном числе. 
+// Если число отрицательное, метод возвращает 0
+
+int NumberOfDigits(int num)
+{
+    int count = 0;
+    if (num >= 0)
+    {
+        count = 1;
+        while (num > 9)
+        {
+            num = num / 10;
+            count++;
+        }
+    }
+    return count;
+}
