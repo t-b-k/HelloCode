49e24d1b82c67510d2cf08aa0fe8d3873b3ce9f8
Tatiana <t-b-k@bk.ru>
2022-12-09 17:39:54 +0300

    Tried to add Common

---
+++ Common/Helper.cs
@@ -0,0 +1,24 @@
+﻿namespace Common;
+
+public static class Helper
+{
+    public static void PrintArray (int[] array)
+    {
+        for (var i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            Console.Write(" "); 
+        }
+    }
+
+    public static int[] CreateRandomArray()
+    {
+        Random random = new Random(); 
+        int[] array = new int[10]; 
+        for (var i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(int.MinValue, int.MaxValue); 
+        }
+        return array; 
+    }
+}
