452d86add5cbe3518264048fcb310c6d173d9960
Tatiana <t-b-k@bk.ru>
2022-12-10 14:00:45 +0300

    Added Method InputPositiveInteger to Helper.cs

--- Common/Helper.cs
+++ Common/Helper.cs
@@ -2,23 +2,29 @@
 
 public static class Helper
 {
-    public static void PrintArray (int[] array)
+    public static bool InputPositiveInteger(string invit, out uint n)
+    {
+        Console.Write(invit);
+        bool res = uint.TryParse(Console.ReadLine(), out n) && n != 0;
+        return res;
+    }
+    public static void PrintArray(int[] array)
     {
         for (var i = 0; i < array.Length; i++)
         {
             Console.Write(array[i]);
-            Console.Write(" "); 
+            Console.Write(" ");
         }
     }
 
     public static int[] CreateRandomArray()
     {
-        Random random = new Random(); 
-        int[] array = new int[10]; 
+        Random random = new Random();
+        int[] array = new int[10];
         for (var i = 0; i < array.Length; i++)
         {
-            array[i] = random.Next(int.MinValue, int.MaxValue); 
+            array[i] = random.Next(int.MinValue, int.MaxValue);
         }
-        return array; 
+        return array;
     }
 }
