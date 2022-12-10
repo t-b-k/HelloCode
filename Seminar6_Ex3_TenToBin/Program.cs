f98df05ecd316621df1d32d12ecf3bdb4237aeac
Tatiana <t-b-k@bk.ru>
2022-12-10 16:01:57 +0300

    Added Example 3 to Seminar 6 - print binary presentation of decimal number input by user

---
+++ Seminar6_Ex3_TenToBin/Program.cs
@@ -0,0 +1,53 @@
+﻿using static Common.Helper; 
+
+// Напишите программу, которая будет преобразовывать 
+// целое десятичное число в двоичное
+
+uint decNumber = 0; 
+
+if (!InputPositiveInteger("Введите целое положительное число => ", out decNumber)) 
+{
+    Console.WriteLine("\nОшибка ввода. Программа завершает свою работу. \n");
+    return;  
+}
+
+uint binArrayLength = numberOfBinRanges(decNumber);  
+
+Console.WriteLine("Двоичная запись введенного Вами числа будет выглядеть так: \n"); 
+Console.WriteLine(string.Join("",CreateBinArrayFromDecimalNumber(decNumber))); 
+
+uint[] CreateBinArrayFromDecimalNumber (uint number)
+{
+    uint arrayLength = numberOfBinRanges(number); 
+    uint[] binNumberArray = new uint[arrayLength]; 
+
+    for (int i = (int)arrayLength-1; i >= 0; i--)
+    {
+        binNumberArray[i] = number % 2; 
+        number = number / 2; 
+    }
+    return binNumberArray; 
+}
+
+void PrintBinDigitsArray (uint[] binArray)
+{
+    Console.WriteLine(); 
+    for (int i = 0; i < binArray.Length; i++) 
+    {
+        Console.Write(binArray[i]); 
+    }
+    Console.WriteLine(); 
+} 
+
+uint numberOfBinRanges (uint decNumber) 
+{
+    uint res = 1;  
+    decNumber = decNumber / 2; 
+
+    while (decNumber > 0) 
+    {
+        res++; 
+        decNumber = decNumber / 2; 
+    }
+    return res; 
+}
