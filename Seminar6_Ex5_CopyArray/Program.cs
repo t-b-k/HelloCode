ed5568b65073e4ebe03d0aa75f9172987c769486
Tatiana <t-b-k@bk.ru>
2022-12-10 17:50:04 +0300

    Added Example 5 from Seminar 6 - copying of the given array by elements

---
+++ Seminar6_Ex5_CopyArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using static Common.Helper; 
+
+// Напишите программу, которая создает копию заданного 
+// массива с помощью поэлементного копирования
+
+int[] array = new int[]{1,3,4,5,3,7,7,4,565,4}; 
+
+PrintArray(array);
+Console.WriteLine(); 
+
+int length = array.Length; 
+
+int[] duplicatedArray = new int[length]; 
+
+for (int i = 0; i <= length - 1; i++) 
+{
+    duplicatedArray[i] = array[i]; 
+}
+
+Console.WriteLine("\nКопия выглядит так: \n");
+PrintArray(duplicatedArray);
+
+
+
