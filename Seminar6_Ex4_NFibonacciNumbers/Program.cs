7138a6d11dab7a57e11309db83ba11f5eb64039b
Tatiana <t-b-k@bk.ru>
2022-12-10 17:14:21 +0300

    Added Example 4 from Seminar 6 - Output of N first Fibonacci numbers, N is input by user

---
+++ Seminar6_Ex4_NFibonacciNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using static Common.Helper; 
+
+// Не используя рекурсию, выведите на экран первые N 
+// чисел Фибоначчи. Первые два числа равны 0 и 1. 
+
+uint quantityOfFibonacciNumbers = 0; // В эту переменную будем считывать введенное пользователем N
+
+if (!InputPositiveInteger("Укажите, какое количество чисел Фибоначчи "+
+                          "Вы хотите увидеть => ", out quantityOfFibonacciNumbers))
+{
+    Console.WriteLine("Ошибка ввода. Программа завершает работу. \n");
+    return;  
+}
+
+uint[] arrayOfFibonacciNumbers = new uint[quantityOfFibonacciNumbers]; 
+
+FillArrayWithFibonacciNumbers(arrayOfFibonacciNumbers); 
+
+Console.WriteLine($"{quantityOfFibonacciNumbers} первых чисел Фибоначчи выглядят так: \n");
+Console.WriteLine(string.Join(" ", arrayOfFibonacciNumbers));  
+Console.WriteLine(); 
+
+void FillArrayWithFibonacciNumbers (uint[] array)
+{
+    int qty = array.Length; 
+    array[0] = 0; 
+
+    if (qty > 1) 
+    {
+        array[1] = 1; 
+        for (int i = 2; i < qty; i++)
+        {
+        array[i] = array[i-2] + array[i-1]; 
+        }
+    }
+}
+
