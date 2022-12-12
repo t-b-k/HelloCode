773574682c271562e1f6a87e82af5e2ca6742c83
Tatiana <t-b-k@bk.ru>
2022-12-12 22:25:07 +0300

    Added Example 4 to Seminar 7 - sum of diagonal elements

---
+++ Seminar7_Ex4_DiagElementsSum/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найдите сумму диагональных элементов массива nxn 
+
+using static Common.Helper; 
+
+int n = 5; 
+
+double[,] ourArray = Create2DArray(n, n);
+
+Print2DArray(ourArray); 
+
+double sumOfDiagElements = SumOfDiagElements(ourArray); 
+
+Console.WriteLine(sumOfDiagElements); 
+
+double SumOfDiagElements (double[,] arr)
+{
+    double result = 0; 
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+       result = result + arr[i, i]; 
+    }
+    return result; 
+}
