37da97f8bc825e8c226290402041517fbcc62c6c
Tatiana <t-b-k@bk.ru>
2022-12-12 21:59:31 +0300

    Added Create 2D array method for double

--- Common/Helper.cs
+++ Common/Helper.cs
@@ -92,4 +92,16 @@ public static class Helper
         }
     }
 
+    public static void Print2DArray(double[,] array)
+    {
+        for (var i = 0; i < array.GetLength(0); i++)
+        {
+            for (var j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();             
+        }
+    }
+
 }
