da9c0f72a2a1a7d760e263db8080fb45821b4663
Tatiana <t-b-k@bk.ru>
2022-12-16 15:01:46 +0300

    Added Example 2 to Seminar 8 - strings and columns exchange

---
+++ Seminar8_Ex2_StringsColumnsExchange/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте двумерный массив. 
+// Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна 
+// вывести сообщение для пользователя
+
+using static Common.Helper; 
+
+int numberOfRows = 0; 
+int numberOfColumns = 0; 
+uint maxRows = 10; 
+uint maxColumns = 10; 
+int minValue = -50; 
+int maxValue = 50; 
+
+double[,] array = CreateRandom2DDoubleArray(out numberOfRows, out numberOfColumns,
+                                            maxRows, maxColumns,
+                                            minValue, maxValue); 
+
+double[,] turnedArray = new double[numberOfColumns, numberOfRows];
+
+for (int i = 0; i < numberOfRows; i++)
+{
+    for (int j = 0; j < numberOfColumns; j++)
+    {
+        turnedArray[j,i] = array[i,j]; 
+    }
+}
+
+Print2DArray(array, 2); 
+Console.WriteLine(); 
+Print2DArray(turnedArray, 2); 
+Console.WriteLine(); 
+
