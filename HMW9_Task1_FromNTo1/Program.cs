12b0490e1f9abd9613d44375322bf67031fadb0a
Tatiana <t-b-k@bk.ru>
2022-12-21 17:36:51 +0300

    Task1 - Printing numbers from N to 1 by descending is solved

---
+++ HMW9_Task1_FromNTo1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+using static Common.Helper;
+
+// Ввод натурального числа N
+
+uint n = 1;
+uint m = 1;
+
+Console.WriteLine("Программа выводит на экран последовательно в порядке убывания ");
+Console.WriteLine("все натуральные числа от N до {m} \n");
+
+if (InputPositiveInteger("Введите натуральное число N => ", out n))
+{
+    PrintByDescender(n,m); 
+}
+else
+{
+    Console.WriteLine("Введенное Вами число не является натуральным!\n"); 
+}
+
+// void PrintByDescender(uint n, uint m)
+// {
+//     if (n >= m)
+//     {
+//         if (n == m) Console.WriteLine(n);
+//         else
+//         {
+//             Console.Write($"{n}, ");
+//             PrintByDescender(n - 1, m);
+//         }
+//         return;
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Печатать нечего, извините: {n} < {m}\n"); 
+//     }
+// }
