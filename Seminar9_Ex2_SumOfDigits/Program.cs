a536cfdc60eb46b355b04673f4b4d54d4f13439a
Tatiana <t-b-k@bk.ru>
2022-12-21 16:11:04 +0300

    Added Example 2 - printing out of all numbers from a to N and from M to N

---
+++ Seminar9_Ex2_SumOfDigits/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая будет принимать на вход число
+// и возвращать сумму его цифр
+
+using static Common.Helper;
+
+int number = 0; 
+
+if (!InputPositiveInteger("Введите положительное целое => ", out number))
+{   
+    Console.WriteLine("Вы не справились с вводом числа! |\n"); 
+    return; 
+}
+else
+{
+    Console.WriteLine($"Сумма цифр числа {number} равна {SumOfDigits(number)}\n"); 
+}
+
+int SumOfDigits (int n)
+{
+    if (n / 10 == 0) 
+    {
+        return n; 
+    }
+    else
+    {
+        return SumOfDigits(n/10) + n%10; 
+    }
+}
+
+
+
+
+
